95393d291386ec0f8f719bd78f74db13f9b439e2
vineetmurari <68270056+vineetmurari@users.noreply.github.com>
2020-11-16 13:36:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, Image, StyleSheet, Button,Alert } from 'react-native';
 
-export default function App() {
+
+
+const App = () => {
+  const [Username, setUsername] = useState('');
+  const [password, setpassword] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<React.Fragment>
+<View style={styles.container}>
+  <Text    style={{fontWeight: 'bold', padding: 5, fontFamily: 'monospace'}}>Online Fruits & Veggies</Text>
+  <Image source={require('./assets/Logo.jpg')}
+style={{  resizeMode: "cover",
+height: 100,
+width: 100
+}}
+/>
+
+<View style={styles.separator} />
+<Text  style={{fontWeight: 'bold', padding: 5, fontFamily: 'monospace'}}> Automate with Me - Demo Mobile App </Text>
+
+</View>
+    <View style={{padding: 75}}>
+      <TextInput
+        style={{height: 30}}
+        placeholder="Username *"
+        defaultValue={Username}
+        style={{padding: 10}}
+        onChangeText={text => setUsername(text)}
+      />
+
+<View style={styles.separator} />
+
+      <TextInput
+        style={{height: 20}}
+        secureTextEntry={true}
+        placeholder="Password *"
+        defaultValue={password}
+        style={{padding: 10}}
+        onChangeText={text => setpassword(text)}
+      />
+
+<View style={styles.separator} />
+
+        <View style={{paddingTop: 30, paddingBottom: 0}}>        
+        <Button
+           title="Sign In"
+            color="#F99D81"
+            accessibilityLabel="Learn more about this purple button"
+            onPress={()=>{if(Username=='' || password=='' ){
+              Alert.alert(`Please fill the details '*' mark fields`)
+            }
+            else if(Username=='Online_User' && password=='Online_User'){
+              Alert.alert('Succeess')
+            }
+          else{
+            Alert.alert('Invalid User Credentials')
+          }
+        }}
+          />
+</View>
+
+
     </View>
+
+
+    
+    <View style={styles.separator} />
+    <View style={styles.container1}>
+    <Text style={styles.title}>
+    Valid User : Online_User and Valid Password: Online_User
+     </Text>
+    <Text style={styles.title}>
+**** This site developed for Demo purpose and utilized for Automation testing ****
+    </Text>
+    <Text style={styles.title}>
+    Developer : Vineet
+    </Text>
+
+    </View>
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "30%",
+    alignItems: "center",
+    height: "30%",
+    textAlign: "center",
+    backgroundColor:'#FBDFDF',
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    },
   },
+  container1: {
+    backgroundColor:'#F9F3A6',
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    },
+  },
+  separator: {
+    marginVertical: 4,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+
+  title: {
+    textAlign: 'center',
+    marginVertical: 1,
+    color: '#b22222',
+  }
+
 });
+
+
+
+
+export default App;
---
+++ Checkout.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View, Image, StyleSheet, Button,Alert } from 'react-native';
+
+const checkout = () => {
+    const [Username, setUsername] = useState('');
+    const [password, setpassword] = useState('');
+    return (
+  console.log()
+    );
+  }
+
+  export default checkout;
