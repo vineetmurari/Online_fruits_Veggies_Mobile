fd51253b31f641be7b7f3119a198661a8a59d70a
vineetmurari <68270056+vineetmurari@users.noreply.github.com>
2020-11-17 12:04:41 +0530

    third commit

--- Products.js
+++ Products.js
@@ -5,13 +5,48 @@ import Images from './imagecollection.js';
 
 const products = () => {
     
-    const [items, setitems]= useState([{"item": "Apple", "Price":"5","Qty":"0", "Desc":"Fresh Apple from Simla","path":"1"}
-  ,{"item": "Banana", "Price":"3","Qty":"0", "Desc":"Fresh and clean Banana bought directly from the plant","path":"2"},
-  {"item": "Fresh Orange", "Price":"5","Qty":"0", "Desc":"Clean Orange from Farm","path":"4"},
-  {"item": "Brinjal", "Price":"5","Qty":"0", "Desc":"Organic veggie - Brinjal","path":"3"},
-  {"item": "Fresh potato", "Price":"5","Qty":"0", "Desc":"Organic veggie","path":"5"}]);
+    const [items, setitems]= useState([{"id":"0","item": "Apple", "Price":"5",Qty:0, "Desc":"Fresh Apple from Simla","path":"1"}
+  ,{"id":"1","item": "Banana", "Price":"3",Qty:0, "Desc":"Fresh and clean Banana bought directly from the plant","path":"2"},
+  {"id":"2","item": "Fresh Orange", "Price":"5",Qty:0, "Desc":"Clean Orange from Farm","path":"4"},
+  {"id":"3","item": "Brinjal", "Price":"5",Qty:0, "Desc":"Organic veggie - Brinjal","path":"3"},
+  {"id":"4","item": "Fresh potato", "Price":"5",Qty:0, "Desc":"Organic veggie","path":"5"}]);
+
+  const [tot, settot]=useState(0);
+
+  const additams = index => e => {
+
+    console.log('index: ' + index);
+    let newArr = [...items]; // copying the old datas array
+    newArr[index].Qty = newArr[index].Qty+1; // replace e.target.value with whatever you want to change it to
+
+    setitems(newArr); 
+    settot(tot+1)
+}
+const delitems = index => e => {
+
+  console.log('index: ' + index);
+  let newArr = [...items]; // copying the old datas array
+ if(!newArr[index].Qty==0){
+  newArr[index].Qty = newArr[index].Qty-1; // replace e.target.value with whatever you want to change it to
+ }
+  setitems(newArr); 
+  settot(tot+1)
+} 
+
 
     return (
+      <React.Fragment>
+        <View  style={{flexDirection:'row', flexWrap:'wrap', backgroundColor:'#D3D0FE'}}>
+          <Text style={{padding:20}}>
+            <Button title='Logout'/>
+          </Text>
+          <Text style={{paddingTop:20,paddingBottom:20, paddingLeft:10, paddingRight:20}}>
+            Total Price : ${tot}
+          </Text>
+          <Text style={{paddingLeft:3, paddingTop:20, paddingBottom:20}}>
+            <Button title='Checkout'/>
+          </Text>
+        </View>
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
        {
          
@@ -23,34 +58,27 @@ const products = () => {
                               </CardItem>
                               <CardItem>
                                 <Body>
-                                 <Image source={Images[y.path]} style={styles.tinyLogo}/>
-                                  <Text>
+                                 <Image source={Images[y.path]} style={styles.tinyLogo} />
+                                  <Text style={{padding:20}}>
                                     {"\n"}
-                                    Price: $ {y.Price}
+                                    Price: ${y.Price}
                                     {"\n"}
-                                    Qty:  {y.Qty}
+                                    Quantity:  {y.Qty}
                                     {"\n"}
                                   </Text>
                                   <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-                                  <View style={{ height: 100, marginTop: 10 }}>
-                                  <Button
-                                    title="+"
-                                    color="#640D83"
-                                    />
-                                    <Text>
-                                      {
-                                        "     "
-                                      }
-                                    </Text>
-                                    </View>
-                                    <View style={{ height: 100, marginTop: 10 }}>
-                                    <Button
-                                    title="-"
-                                    color="#640D83"
+                                  
+                                  <Button title="Add items (+)"
+                                  onPress={additams(y.id)}
 
-                                    />
-                                    </View>
-                                    </View>
+                                  />
+                                  <Text>
+                                    {"               "}
+                                  </Text>
+                                  <Button title="Remove items (-)"
+                                  onPress={delitems(y.id)}/>
+                               
+                                  </View>
                                 </Body>
                               </CardItem>
                               <CardItem footer>
@@ -62,6 +90,7 @@ const products = () => {
                        
                     }
         </ScrollView>
+        </React.Fragment>
     );
   }
 
@@ -69,9 +98,11 @@ const products = () => {
   const styles = StyleSheet.create({
     
     tinyLogo: {
-      width: 220,
-      height: 150,
+      height: 100*2,
+      width: 100*2,
+      resizeMode: "contain"
     }
   });
+
 
   export default products;
